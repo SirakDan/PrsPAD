9a3497241e5b97476040f7c107a08bcbcdd36813
SirakDan <dansirak@gmail.com>
2014-03-19 13:10:07 +0100

    Avances
    Añadidos activities, terminado el principal y añadidas transiciones
    entre activities con sus XMLs correspondientes.

--- PAD1/gen/com/example/pad1/R.java
+++ PAD1/gen/com/example/pad1/R.java
@@ -8,10 +8,16 @@
 package com.example.pad1;
 
 public final class R {
+    public static final class anim {
+        public static final int left_in=0x7f040000;
+        public static final int left_out=0x7f040001;
+        public static final int right_in=0x7f040002;
+        public static final int right_out=0x7f040003;
+    }
     public static final class array {
-        public static final int citiesPortugal=0x7f060002;
-        public static final int citiesSpain=0x7f060001;
-        public static final int countries=0x7f060000;
+        public static final int countries=0x7f070000;
+        public static final int nationalities=0x7f070001;
+        public static final int typeFieldArray=0x7f070002;
     }
     public static final class attr {
     }
@@ -22,32 +28,54 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f090004;
-        public static final int editText1=0x7f090001;
-        public static final int spinnerCities=0x7f090002;
-        public static final int spinnerCountries=0x7f090003;
-        public static final int textView1=0x7f090000;
+        public static final int action_settings=0x7f0a000c;
+        public static final int backButton=0x7f0a000b;
+        public static final int cityText=0x7f0a0001;
+        public static final int countryText=0x7f0a0002;
+        public static final int nameText=0x7f0a0000;
+        public static final int nationalitiesSpinner=0x7f0a0008;
+        public static final int noNumberCheckbox=0x7f0a0006;
+        public static final int numberText=0x7f0a0005;
+        public static final int priceText=0x7f0a000a;
+        public static final int roadCheckbox=0x7f0a0003;
+        public static final int searchButton=0x7f0a0009;
+        public static final int streetText=0x7f0a0004;
+        public static final int typesSpinner=0x7f0a0007;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int activity_results=0x7f030001;
     }
     public static final class menu {
-        public static final int main=0x7f080000;
+        public static final int main=0x7f090000;
+        public static final int results=0x7f090001;
     }
     public static final class string {
-        public static final int action_settings=0x7f050001;
-        public static final int app_name=0x7f050000;
-        public static final int resCountry=0x7f050004;
-        public static final int resName=0x7f050003;
-        public static final int spinnerPrompt=0x7f050005;
-        public static final int title=0x7f050002;
+        public static final int action_settings=0x7f060001;
+        public static final int app_name=0x7f060000;
+        public static final int back=0x7f06000f;
+        public static final int city=0x7f060009;
+        public static final int country=0x7f060007;
+        public static final int hello_world=0x7f060011;
+        public static final int noNumber=0x7f06000d;
+        public static final int number=0x7f06000c;
+        public static final int price=0x7f060008;
+        public static final int resCountry=0x7f060004;
+        public static final int resName=0x7f060003;
+        public static final int road=0x7f06000b;
+        public static final int search=0x7f06000e;
+        public static final int spinnerPrompt=0x7f060005;
+        public static final int street=0x7f06000a;
+        public static final int title=0x7f060002;
+        public static final int title_activity_results=0x7f060010;
+        public static final int typeFieldName=0x7f060006;
     }
     public static final class style {
         /** 
@@ -71,10 +99,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f070000;
+        public static final int AppBaseTheme=0x7f080000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f070001;
+        public static final int AppTheme=0x7f080001;
     }
 }
--- PAD1/src/com/example/pad1/MainActivity.java
+++ PAD1/src/com/example/pad1/MainActivity.java
@@ -1,15 +1,31 @@
 package com.example.pad1;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+    	Log.d("Spinner", "Spinner pulsado");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button search = (Button) findViewById(R.id.searchButton);
+        search.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Intent changeWindow = new Intent(MainActivity.this, Results.class);
+				startActivity(changeWindow);
+				overridePendingTransition(R.anim.left_in, R.anim.left_out);
+			}
+		});
     }
 
 
@@ -20,4 +36,28 @@ public class MainActivity extends Activity {
         return true;
     }
     
+    
+    
+    
+    
+    /*
+    Spinner countries = (Spinner) findViewById(R.id.spinnerCountries);
+    Spinner cities = (Spinner) findViewById(R.id.spinnerCities);
+    
+    
+    countries.
+    
+	 // Create an ArrayAdapter using the string array and a default spinner layout
+	 ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+	         R.array.planets_array, android.R.layout.simple_spinner_item);
+ // Specify the layout to use when the list of choices appears
+ adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+ // Apply the adapter to the spinner
+ spinner.setAdapter(adapter);*/
+    
+    
 }
+
+
+
+
