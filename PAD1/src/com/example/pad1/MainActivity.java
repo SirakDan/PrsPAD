40bd23926744d72095a2444a74a2131f979df7ec
AdrianVader <adri.,rabadan@gmail.com>
2014-03-20 19:32:49 +0100

    Ya coge lo que tienen los botones
    ASDF falta meter el Spinner del tipo de calle

--- PAD1/src/com/example/pad1/MainActivity.java
+++ PAD1/src/com/example/pad1/MainActivity.java
@@ -8,6 +8,9 @@ import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 public class MainActivity extends Activity {
 
@@ -24,6 +27,31 @@ public class MainActivity extends Activity {
 				Intent changeWindow = new Intent(MainActivity.this, Results.class);
 				startActivity(changeWindow);
 				overridePendingTransition(R.anim.left_in, R.anim.left_out);
+				
+				// Los filtros de la interfaz.
+				EditText restaurantName = (EditText) findViewById(R.id.nameText);
+				EditText country = (EditText) findViewById(R.id.countryText);
+				EditText city = (EditText) findViewById(R.id.cityText);
+				EditText street = (EditText) findViewById(R.id.streetText);
+				//CheckBox isARoad = (CheckBox) findViewById(R.id.roadCheckbox);
+				EditText streetNumber = (EditText) findViewById(R.id.numberText);
+				CheckBox noNumber = (CheckBox) findViewById(R.id.noNumberCheckbox);
+				Spinner type = (Spinner) findViewById(R.id.typesSpinner); // Creativa | Tradicional.
+				Spinner nationality = (Spinner) findViewById(R.id.nationalitiesSpinner);
+				EditText price = (EditText) findViewById(R.id.priceText);
+				
+				Log.v("hola !!!!!!!!!", type.getSelectedItem().toString());
+				Restaurant r = new Restaurant(restaurantName.getText().toString()
+											, country.getText().toString()
+											, city.getText().toString()
+											, "LocationType Desconocido (asdf, falta en la interfaz)"
+											, street.getText().toString()
+											, noNumber.isChecked()
+											, Integer.parseInt(streetNumber.getText().toString())
+											, type.getSelectedItem().toString()
+											, nationality.getSelectedItem().toString()
+											, Float.parseFloat(price.getText().toString()));
+				
 			}
 		});
     }
