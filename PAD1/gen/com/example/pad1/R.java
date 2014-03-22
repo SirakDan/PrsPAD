7ef267c58831f25c49771dfd4e28a8cc4c6a9363
liith <teresuky7@gmail.com>
2014-03-22 14:13:02 +0100

    en proceso de mezclar bien el código pero...
    No se puede acceder al priceBar desde el onClick, hay que solucionarlo

--- PAD1/gen/com/example/pad1/R.java
+++ PAD1/gen/com/example/pad1/R.java
@@ -36,19 +36,20 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f0a000d;
-        public static final int backButton=0x7f0a000b;
+        public static final int action_settings=0x7f0a000e;
+        public static final int backButton=0x7f0a000c;
         public static final int cityText=0x7f0a0001;
         public static final int countryText=0x7f0a0002;
         public static final int nameText=0x7f0a0000;
         public static final int nationalitiesSpinner=0x7f0a0006;
-        public static final int noNumberCheckbox=0x7f0a0009;
+        public static final int noNumberCheckbox=0x7f0a0008;
         public static final int numberText=0x7f0a0003;
-        public static final int priceText=0x7f0a0008;
-        public static final int resultadoArea=0x7f0a000c;
+        public static final int priceBar=0x7f0a000b;
+        public static final int priceTextView2=0x7f0a000a;
+        public static final int resultadoArea=0x7f0a000d;
         public static final int searchButton=0x7f0a0007;
         public static final int streetText=0x7f0a0004;
-        public static final int tipeOfRoadSinner=0x7f0a000a;
+        public static final int tipeOfRoadSinner=0x7f0a0009;
         public static final int typesSpinner=0x7f0a0005;
     }
     public static final class layout {
@@ -62,21 +63,22 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f060001;
         public static final int app_name=0x7f060000;
-        public static final int back=0x7f06000f;
-        public static final int city=0x7f060009;
+        public static final int back=0x7f060010;
+        public static final int city=0x7f06000a;
         public static final int country=0x7f060007;
-        public static final int hello_world=0x7f060011;
-        public static final int noNumber=0x7f06000d;
-        public static final int number=0x7f06000c;
+        public static final int hello_world=0x7f060012;
+        public static final int noNumber=0x7f06000e;
+        public static final int number=0x7f06000d;
         public static final int price=0x7f060008;
+        public static final int priceText=0x7f060009;
         public static final int resCountry=0x7f060004;
         public static final int resName=0x7f060003;
-        public static final int road=0x7f06000b;
-        public static final int search=0x7f06000e;
+        public static final int road=0x7f06000c;
+        public static final int search=0x7f06000f;
         public static final int spinnerPrompt=0x7f060005;
-        public static final int street=0x7f06000a;
+        public static final int street=0x7f06000b;
         public static final int title=0x7f060002;
-        public static final int title_activity_results=0x7f060010;
+        public static final int title_activity_results=0x7f060011;
         public static final int typeFieldName=0x7f060006;
     }
     public static final class style {
--- PAD1/src/com/example/pad1/MainActivity.java
+++ PAD1/src/com/example/pad1/MainActivity.java
@@ -11,147 +11,155 @@ import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
-<<<<<<< HEAD
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Spinner;
-=======
 import android.widget.SeekBar;
 import android.widget.SeekBar.OnSeekBarChangeListener;
 import android.widget.TextView;
->>>>>>> origin/Experimentos-visuales-de-Teresa
 
-public class MainActivity extends Activity /*implements OnSeekBarChangeListener */{
+public class MainActivity extends Activity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-    	Log.d("Spinner", "Spinner pulsado");
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Button search = (Button) findViewById(R.id.searchButton);
-        search.setOnClickListener(new OnClickListener() {
+	    	Log.d("Spinner", "Spinner pulsado");
+	        super.onCreate(savedInstanceState);
+	        setContentView(R.layout.activity_main);
+	        Button search = (Button) findViewById(R.id.searchButton);
+	        SeekBar priceBar = (SeekBar) findViewById(R.id.priceBar);
+	        
+	        search.setOnClickListener(new OnClickListener() {
 			
-			@Override
-			public void onClick(View v) {
-				Intent changeWindow = new Intent(MainActivity.this, Results.class);
+					@Override
+					public void onClick(View v) {
+							Intent changeWindow = new Intent(MainActivity.this, Results.class);
+							
+							overridePendingTransition(R.anim.left_in, R.anim.left_out);
+							// Los filtros de la interfaz.
+							EditText restaurantName = (EditText) findViewById(R.id.nameText);
+							String restaurantNameValue = restaurantName.getText().toString();
+							
+							EditText country = (EditText) findViewById(R.id.countryText);
+							String countryValue = country.getText().toString();
 				
-				overridePendingTransition(R.anim.left_in, R.anim.left_out);
-				// Los filtros de la interfaz.
-				EditText restaurantName = (EditText) findViewById(R.id.nameText);
-				String restaurantNameValue = restaurantName.getText().toString();
+							EditText city = (EditText) findViewById(R.id.cityText);
+							String cityValue = city.getText().toString();
+							
+							EditText street = (EditText) findViewById(R.id.streetText);
+							String streetValue = street.getText().toString();
+							
+							Spinner streetType = (Spinner) findViewById(R.id.tipeOfRoadSinner);
+							String streetTypeValue;
+							if(streetType.getSelectedItemPosition() == 0){
+								streetTypeValue = "";
+							}else{
+								streetTypeValue = streetType.getSelectedItem().toString();
+							}
 				
-				EditText country = (EditText) findViewById(R.id.countryText);
-				String countryValue = country.getText().toString();
+							EditText streetNumber = (EditText) findViewById(R.id.numberText);
+							int streetNumberValue;
+							if(!streetNumber.getText().toString().equals("")){
+								streetNumberValue = Integer.parseInt(streetNumber.getText().toString());
+							}else{
+								streetNumberValue = -1;
+							}
 				
-				EditText city = (EditText) findViewById(R.id.cityText);
-				String cityValue = city.getText().toString();
+							CheckBox noNumber = (CheckBox) findViewById(R.id.noNumberCheckbox);
+							Boolean noNumberValue = noNumber.isChecked();
+							
+							Spinner type = (Spinner) findViewById(R.id.typesSpinner); // Creativa | Tradicional.
+							String typeValue;
+							if(type.getSelectedItemPosition() == 0){
+								typeValue = "";
+							}else{
+								typeValue = type.getSelectedItem().toString();
+							}
 				
-				EditText street = (EditText) findViewById(R.id.streetText);
-				String streetValue = street.getText().toString();
+							Spinner nationality = (Spinner) findViewById(R.id.nationalitiesSpinner);
+							String nationalityValue;
+							if(nationality.getSelectedItemPosition() == 0){
+								nationalityValue = "";
+							}else{
+								nationalityValue = nationality.getSelectedItem().toString();
+							}
+							/*
+							EditText price = (EditText) findViewById(R.id.priceText);
+							float priceValue;
+							if(!price.getText().toString().equals("")){
+								priceValue = Float.parseFloat(price.getText().toString());
+							}else{
+								priceValue = -1;
+							}*/
+							//EditText price = (EditText) findViewById(R.id.priceText);
+							float priceValue;
+							if(priceBar.getProgress() != 0){
+								priceValue = priceBar.getProgress();
+							}else{
+								priceValue = -1;
+							}
 				
-				Spinner streetType = (Spinner) findViewById(R.id.tipeOfRoadSinner);
-				String streetTypeValue;
-				if(streetType.getSelectedItemPosition() == 0){
-					streetTypeValue = "";
-				}else{
-					streetTypeValue = streetType.getSelectedItem().toString();
-				}
-				
-				EditText streetNumber = (EditText) findViewById(R.id.numberText);
-				int streetNumberValue;
-				if(!streetNumber.getText().toString().equals("")){
-					streetNumberValue = Integer.parseInt(streetNumber.getText().toString());
-				}else{
-					streetNumberValue = -1;
-				}
-				
-				CheckBox noNumber = (CheckBox) findViewById(R.id.noNumberCheckbox);
-				Boolean noNumberValue = noNumber.isChecked();
-				
-				Spinner type = (Spinner) findViewById(R.id.typesSpinner); // Creativa | Tradicional.
-				String typeValue;
-				if(type.getSelectedItemPosition() == 0){
-					typeValue = "";
-				}else{
-					typeValue = type.getSelectedItem().toString();
-				}
-				
-				Spinner nationality = (Spinner) findViewById(R.id.nationalitiesSpinner);
-				String nationalityValue;
-				if(nationality.getSelectedItemPosition() == 0){
-					nationalityValue = "";
-				}else{
-					nationalityValue = nationality.getSelectedItem().toString();
-				}
-				
-				EditText price = (EditText) findViewById(R.id.priceText);
-				float priceValue;
-				if(!price.getText().toString().equals("")){
-					priceValue = Float.parseFloat(price.getText().toString());
-				}else{
-					priceValue = -1;
-				}
 
-				//Log.v("Hola !!!!!!!!!!!!", Integer.toString(streetType.getSelectedItemPosition()));
-				Restaurant r = new Restaurant(restaurantNameValue
-											, countryValue
-											, cityValue
-											, streetTypeValue
-											, streetValue
-											, noNumberValue
-											, streetNumberValue
-											, typeValue
-											, nationalityValue
-											, priceValue);
+							//Log.v("Hola !!!!!!!!!!!!", Integer.toString(streetType.getSelectedItemPosition()));
+							Restaurant r = new Restaurant(restaurantNameValue
+														, countryValue
+														, cityValue
+														, streetTypeValue
+														, streetValue
+														, noNumberValue
+														, streetNumberValue
+														, typeValue
+														, nationalityValue
+														, priceValue);
 				
-				RestaurantFinder restFindr = new RestaurantFinder();
-				restFindr.iniciaRestaurantes();
-				ArrayList<Restaurant> restaurants = restFindr.buscaRestaurante(r);
-				Bundle b = new Bundle();
-				b.putParcelableArrayList("restaurants", (ArrayList<? extends Parcelable>) restaurants);
-				changeWindow.putExtra("restaurants", b);
-				startActivity(changeWindow);
+							RestaurantFinder restFindr = new RestaurantFinder();
+							restFindr.iniciaRestaurantes();
+							ArrayList<Restaurant> restaurants = restFindr.buscaRestaurante(r);
+							Bundle b = new Bundle();
+							b.putParcelableArrayList("restaurants", (ArrayList<? extends Parcelable>) restaurants);
+							changeWindow.putExtra("restaurants", b);
+							startActivity(changeWindow);
 				
 				
-			}
-		});
+					}//end of onClick
+					
+	        });//end of search.setOnClickListener
         
         
-        //-----------PRICE SEEK BAR----------------------------------------------------------------------------
-        
-        SeekBar priceBar = (SeekBar) findViewById(R.id.priceBar);        
-       
-        priceBar.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
+	        //-----------PRICE SEEK BAR----------------------------------------------------------------------------    
+	       
+	        priceBar.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
 
-            TextView priceText = (TextView) findViewById(R.id.priceTextView2);
-            String auxPrice = getResources().getString(R.string.price);
-            String anyPrice = getResources().getString(R.string.priceText);
+		            TextView priceText = (TextView) findViewById(R.id.priceTextView2);
+		            String auxPrice = getResources().getString(R.string.price);
+		            String anyPrice = getResources().getString(R.string.priceText);
+
         	
-			@Override
-			public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-				
-				if(progress == 0)
-					priceText.setText(anyPrice);
-				else
-					priceText.setText("" + auxPrice + " " + progress + "€");
-				
-			}
+					@Override
+					public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+						
+							if(progress == 0)
+								priceText.setText(anyPrice);
+							else
+								priceText.setText("" + auxPrice + " " + progress + "€");
+						
+					}//end of onProgressChanged
 
-			@Override
-			public void onStartTrackingTouch(SeekBar seekBar) {
-				// TODO Auto-generated method stub
-				
-			}
+					@Override
+					public void onStartTrackingTouch(SeekBar seekBar) {
+							// TODO Auto-generated method stub
+						
+					}//end of onStartTrackingTouch
 
-			@Override
-			public void onStopTrackingTouch(SeekBar seekBar) {
-				// TODO Auto-generated method stub
-				
-			}
+					@Override
+					public void onStopTrackingTouch(SeekBar seekBar) {
+							// TODO Auto-generated method stub
+						
+					}//end of onStopTrackingTouch
         	
-        });
+					
+	        });//end of proceBar.setOnSeekBarChangeListener
         
-        //----------------------------------------------------------------------------------------------------
+	        //----------------------------------------------------------------------------------------------------
     }
 
 
