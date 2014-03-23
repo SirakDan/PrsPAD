52f97d4218300f89151e5e4addefd96812ec96d2
SirakDan <dansirak@gmail.com>
2014-03-23 18:53:24 +0100

    asdf

--- PAD1/src/com/example/pad1/MainActivity.java
+++ PAD1/src/com/example/pad1/MainActivity.java
@@ -18,40 +18,34 @@ import android.widget.SeekBar;
 import android.widget.SeekBar.OnSeekBarChangeListener;
 import android.widget.TextView;
 
-<<<<<<< HEAD
-public class MainActivity extends Activity{
-
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		Log.d("Spinner", "Spinner pulsado");
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		Button search = (Button) findViewById(R.id.searchButton);
-		final SeekBar priceBar = (SeekBar) findViewById(R.id.priceBar);
-
-		search.setOnClickListener(new OnClickListener() {
-=======
 public class MainActivity extends Activity /*implements OnSeekBarChangeListener */{
->>>>>>> origin/Unstable
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+    	Log.d("Spinner", "Spinner pulsado");
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button search = (Button) findViewById(R.id.searchButton);
+        search.setOnClickListener(new OnClickListener() {
+			
 			@Override
 			public void onClick(View v) {
 				Intent changeWindow = new Intent(MainActivity.this, Results.class);
-
+				
 				overridePendingTransition(R.anim.left_in, R.anim.left_out);
 				// Los filtros de la interfaz.
 				EditText restaurantName = (EditText) findViewById(R.id.nameText);
 				String restaurantNameValue = restaurantName.getText().toString();
-
+				
 				EditText country = (EditText) findViewById(R.id.countryText);
 				String countryValue = country.getText().toString();
-
+				
 				EditText city = (EditText) findViewById(R.id.cityText);
 				String cityValue = city.getText().toString();
-
+				
 				EditText street = (EditText) findViewById(R.id.streetText);
 				String streetValue = street.getText().toString();
-
+				
 				Spinner streetType = (Spinner) findViewById(R.id.tipeOfRoadSinner);
 				String streetTypeValue;
 				if(streetType.getSelectedItemPosition() == 0){
@@ -59,7 +53,7 @@ public class MainActivity extends Activity /*implements OnSeekBarChangeListener 
 				}else{
 					streetTypeValue = streetType.getSelectedItem().toString();
 				}
-
+				
 				EditText streetNumber = (EditText) findViewById(R.id.numberText);
 				int streetNumberValue;
 				if(!streetNumber.getText().toString().equals("")){
@@ -67,10 +61,10 @@ public class MainActivity extends Activity /*implements OnSeekBarChangeListener 
 				}else{
 					streetNumberValue = -1;
 				}
-
+				
 				CheckBox noNumber = (CheckBox) findViewById(R.id.noNumberCheckbox);
 				Boolean noNumberValue = noNumber.isChecked();
-
+				
 				Spinner type = (Spinner) findViewById(R.id.typesSpinner); // Creativa | Tradicional.
 				String typeValue;
 				if(type.getSelectedItemPosition() == 0){
@@ -78,7 +72,7 @@ public class MainActivity extends Activity /*implements OnSeekBarChangeListener 
 				}else{
 					typeValue = type.getSelectedItem().toString();
 				}
-
+				
 				Spinner nationality = (Spinner) findViewById(R.id.nationalitiesSpinner);
 				String nationalityValue;
 				if(nationality.getSelectedItemPosition() == 0){
@@ -86,28 +80,6 @@ public class MainActivity extends Activity /*implements OnSeekBarChangeListener 
 				}else{
 					nationalityValue = nationality.getSelectedItem().toString();
 				}
-<<<<<<< HEAD
-				float priceValue;
-				if(priceBar.getProgress() > 0){
-					priceValue = priceBar.getProgress();
-				}else{
-					priceValue = -1;
-				}
-
-
-				//Log.v("Hola !!!!!!!!!!!!", Integer.toString(streetType.getSelectedItemPosition()));
-				Restaurant r = new Restaurant(restaurantNameValue
-						, countryValue
-						, cityValue
-						, streetTypeValue
-						, streetValue
-						, noNumberValue
-						, streetNumberValue
-						, typeValue
-						, nationalityValue
-						, priceValue);
-
-=======
 				
 				SeekBar sb = (SeekBar) findViewById(R.id.priceBar);
 				float price = sb.getProgress();
@@ -124,7 +96,6 @@ public class MainActivity extends Activity /*implements OnSeekBarChangeListener 
 											, nationalityValue
 											, price);
 				
->>>>>>> origin/Unstable
 				RestaurantFinder restFindr = new RestaurantFinder();
 				restFindr.iniciaRestaurantes();
 				ArrayList<Restaurant> restaurants = restFindr.buscaRestaurante(r);
@@ -132,27 +103,6 @@ public class MainActivity extends Activity /*implements OnSeekBarChangeListener 
 				b.putParcelableArrayList("restaurants", (ArrayList<? extends Parcelable>) restaurants);
 				changeWindow.putExtra("restaurants", b);
 				startActivity(changeWindow);
-<<<<<<< HEAD
-
-
-			}//end of onClick
-
-		});//end of search.setOnClickListener
-
-
-		//-----------PRICE SEEK BAR----------------------------------------------------------------------------    
-
-		priceBar.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
-
-			TextView priceText = (TextView) findViewById(R.id.priceTextView2);
-			String auxPrice = getResources().getString(R.string.price);
-			String anyPrice = getResources().getString(R.string.priceText);
-
-
-			@Override
-			public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-
-=======
 				overridePendingTransition(R.anim.left_in, R.anim.left_out);
 				
 			}
@@ -172,52 +122,22 @@ public class MainActivity extends Activity /*implements OnSeekBarChangeListener 
 			@Override
 			public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
 				
->>>>>>> origin/Unstable
 				if(progress == 0)
 					priceText.setText(anyPrice);
 				else
 					priceText.setText("" + auxPrice + " " + progress + "€");
-<<<<<<< HEAD
-
-			}//end of onProgressChanged
-=======
 				
 			}
->>>>>>> origin/Unstable
 
 			@Override
 			public void onStartTrackingTouch(SeekBar seekBar) {
 				// TODO Auto-generated method stub
-<<<<<<< HEAD
-
-			}//end of onStartTrackingTouch
-=======
 				
 			}
->>>>>>> origin/Unstable
 
 			@Override
 			public void onStopTrackingTouch(SeekBar seekBar) {
 				// TODO Auto-generated method stub
-<<<<<<< HEAD
-
-			}//end of onStopTrackingTouch
-
-
-		});//end of proceBar.setOnSeekBarChangeListener
-
-		//----------------------------------------------------------------------------------------------------
-	}
-
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
-
-=======
 				
 			}
         	
@@ -234,7 +154,6 @@ public class MainActivity extends Activity /*implements OnSeekBarChangeListener 
         return true;
     }
     
->>>>>>> origin/Unstable
 }
 
 
